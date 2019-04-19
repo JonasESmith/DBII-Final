713aad352f922e54fe8b0ab7bc85d9c0899e3e83
Joseph Warren <josephw1225@gmail.com>
2019-04-19 10:27:17 -0500

    A working querry :)

--- WebApplication2/WebApplication2/Pages/Index.cshtml.cs
+++ WebApplication2/WebApplication2/Pages/Index.cshtml.cs
@@ -5,11 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
 
 namespace WebApplication2.Pages
 {
     public class IndexModel : PageModel
     {
+			// For help with the connection see:
+			// https://dev.mysql.com/doc/connector-net/en/connector-net-programming-connecting-connection-string.html
+			MySqlConnection conn;
+
+		public IndexModel()
+		{
+			try
+			{
+				conn = new MySqlConnection();
+				conn.ConnectionString = "server=209.106.201.103;uid=dbstudent24;pwd=greatdugong72;database=group4";
+				
+			}
+			catch (MySql.Data.MySqlClient.MySqlException ex)
+			{
+				//MessageBox.Show(ex.Message);
+			}
+		}
+
         public void OnGet()
         {
 
@@ -21,6 +42,12 @@ namespace WebApplication2.Pages
 			var lastName = Request.Form["lastName"];
 			var address = Request.Form["address"];
 			var gradDate = Request.Form["gradDate"];
+			conn.Open();
+			MySqlCommand cmd = new MySqlCommand();
+			cmd.CommandText = String.Format("insert into TestTable values (NULL, \"{0}\",\"{1}\",\"{2}\",\"{3}\");", firstName, lastName, address, gradDate);
+			cmd.Connection = conn;
+			cmd.CommandType = CommandType.Text;
+			MySqlDataReader reader = cmd.ExecuteReader();
 		}
 	}
 
